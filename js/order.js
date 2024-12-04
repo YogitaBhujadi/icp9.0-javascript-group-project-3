53938315ad63c50a28d96125682ba5276d3a0ca4
ShradhaMane04 <shradhamane2106@gmail.com>
2024-12-04 14:37:23 +0530

    Sidebar Deleted

--- js/order.js
+++ js/order.js
@@ -1,18 +1,3 @@
-// Sidebar
-function openSidebar() {
-    const sidebarElement = document.getElementById('sidebar');
-    sidebarElement.style.visibility = 'visible';
-    const hamburgerMenuElement = document.getElementById('hamburger-menu');
-    hamburgerMenuElement.style.visibility = 'hidden';
-}
-function closeSidebar() {
-    const sidebarElement = document.getElementById('sidebar');
-    sidebarElement.style.visibility = 'hidden';
-    const hamburgerMenuElement = document.getElementById('hamburger-menu');
-    hamburgerMenuElement.style.visibility = 'visible';
-}
-
-
 //Increment Decrement 
 const quantElement = document.getElementById('quantity');
 function inc() {
